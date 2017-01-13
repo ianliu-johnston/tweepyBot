31e9d040e82eeca477a2f9f4f1765a5f0547c44a
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 14:33:23 -0800

    Updated setup, websites, and uploaded an image to tweet

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ pip3 install --upgrade pip
 pip3 install tweepy
 echo "Create the bot script in python, using the tweepy module."
 chmod u+x tweepy_bot.py
+echo "Create another script to run the tweepy bot. With absolute file paths"
 crontab -e
-echo "Enter this at the end of the comments section: 23 20 * * * /home/<user>/tweepy_bot.py
+echo "Enter this at the end of the comments section: 23 17 * * * /home/<user>/run_bot.py >> /home/<user>/tweepyBot/log 2>&1"
