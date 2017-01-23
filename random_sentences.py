99dbf565664305a49fda7b10f1400dd19058210a
Concati <concati@concati-droplet.localdomain>
2017-01-23 10:22:23 +0000

    Added exception handling and error checking

--- random_sentences.py
+++ random_sentences.py
@@ -1,43 +1,69 @@
 #!/usr/bin/python3
 import random
+from datetime import date
 from json import loads
 from misc_tokens import the_word
 from urllib.request import urlopen
 
-def open_files():
+def random_picker(random_or_not):
     options = "two_dolla_words", "websites.txt"
-    random_choice = random.choice(range(len(options)))
-    with open(options[random_choice], 'r') as fr:
-        lst = list(line for line in fr.read().split('\n'))
-        choice = random.choice(range(len(lst)))
-        chosen = lst[choice]
-        del lst[choice]
-    fr.closed
-    with open(options[random_choice], 'w') as fw:
-        for i in lst:
-            fw.write(i)
-            fw.write("\n")
-    fw.closed
-    return(chosen)
+    if random_or_not is True:
+        random_choice = random.choice(range(len(options)))
+    elif random_or_not is -1:
+        random_choice = 1
+    else:
+        random_choice = 0
+    try:
+        with open(options[random_choice], 'r') as fr:
+            lst = list(line for line in fr.read().split('\n'))
+            if len(lst) > 1:
+                choice = random.choice(range(len(lst)))
+                chosen = lst[choice]
+                del lst[choice]
+                with open(options[random_choice], 'w') as fw:
+                    for i in lst:
+                        if len(i) > 1:
+                            fw.write(i)
+                            fw.write("\n")
+                fw.closed
+            else:
+                print("File: {} is completely empty.".format(options[random_choice]))
+                return(None)
+        fr.closed
+        return(chosen)
+    except FileNotFoundError as no_files:
+        print(no_files)
+        return(None)
+
 
 def get_json_parsed(word):
     url = "http://api.wordnik.com:80/v4/word.json/" + word + "/definitions?limit=1&includeRelated=true&useCanonical=false&includeTags=false&api_key=" + the_word
     # url2 = "http://api.wordnik.com:80/v4/words.json/wordOfTheDay?api_key=" + the_word
-    with urlopen(url) as response:
-        data = response.read().decode("utf-8")
-        parsed = loads(data)
-    return(parsed)
+    try:
+        with urlopen(url) as response:
+            data = response.read().decode("utf-8")
+            parsed = loads(data)
+        return(parsed)
+    except Exception as err:
+        print(err)
+        return(-1)
 
 def random_sentence():
+    print("-----------", date.today(), "------------")
     buf = ""
-    word_or_web = open_files()
-    if "http" not in word_or_web:
+    word_or_web = random_picker(True)
+    if word_or_web is None or len(word_or_web) < 1:
+        buf = "I'm out of tweets... #pythoniscool"
+    elif ' ' not in word_or_web:
         buf = "Word Of The Day!\n"
         returned = get_json_parsed(word_or_web)
-        buf += "{}: ".format(returned[0]['word'])
-        buf += "{} -- ".format(returned[0]['partOfSpeech'])
-        buf += "{}".format(returned[0]['text'])
+        if returned != -1:
+            buf += "{}: ".format(returned[0]['word'])
+            buf += "{} -- ".format(returned[0]['partOfSpeech'])
+            buf += "{}".format(returned[0]['text'])
+        else:
+            buf = random_picker(-1)
     else:
         buf += (word_or_web)
+    print(buf)
     return(buf[:140])
-
