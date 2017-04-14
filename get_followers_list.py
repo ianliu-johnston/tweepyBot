013f545df3d0374a8e29fd49529d884a83442431
Concati <concati@concati-droplet.localdomain>
2017-04-14 01:41:32 +0000

    Fixing problem that prevented tweeting today.

---
+++ get_followers_list.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Module to get a list of followers and compare it to my list of following
+"""
+import tweepy
+from tweepy_bot import authenticate
+
+
+    """
+def get_lists(which="followers"):
+    returns a list of followers or following
+    import json
+    api = authenticate()
+    names = []
+    if which == "followers":
+        cursor = tweepy.Cursor(api.followers).items()
+        black_list = []
+    with open('black_list', 'r') as bl:
+        for line in bl:
+            black_list.append(line.strip())
+    elif which == "following":
+        cursor = tweepy.Cursor(api.friends).items()
+        white_list = []
+        with open('white_list', 'r') as bl:
+            for line in bl:
+                white_list.append(line.strip())
+    for user in cursor:
+        if user.following == False and user.screen_name not in black_list:
+            api.create_friendship(user.screen_name)
+    return(names)
+
+###################
+    #Write cursor object to file
+    for user in cursor:
+        with open("friend_cursor_dump", 'a') as f:
+            f.write(json.dumps(user._json))
+
+    #Read cursor object from file
+    with open("cursor_dump") as f:
+        for user in json.loads(f):
+            names.append(user.screen_name)
+    print(names)
+###################
+    """
+
+
+if __name__ == "__main__":
+    followers = get_lists("followers")
+    print(followers)
+#    following = get_lists("following")
+#    print("#############################")
+#    print(followers)
--- tweepy_bot.py
+++ tweepy_bot.py
@@ -1,15 +1,41 @@
 #!/usr/bin/python3
 import tweepy
-from random_sentences import random_sentence
 from misc_tokens import ck, cs, atok, atoks
+"""
+Main Module for twitter bot, includes authentication
+function and default behavior
+"""
 
-SECRET_KEY=ck
-SECRET_SECRET=cs
-SECRET_ACCESS=atok
-SECRET_ACCESS_SECRET=atoks
+"""
+class twitter_bot():
+    Main class with the following functions:
+    authenticate
+    get_followers
+    get_following
+    unfollow
+    follow
+"""
+def authenticate():
+    """
+    Basic authentication that returns the api object
+    """
 
-auth = tweepy.OAuthHandler(SECRET_KEY, SECRET_SECRET)
-auth.set_access_token(SECRET_ACCESS, SECRET_ACCESS_SECRET)
-api = tweepy.API(auth)
+    SECRET_KEY=ck
+    SECRET_SECRET=cs
+    SECRET_ACCESS=atok
+    SECRET_ACCESS_SECRET=atoks
 
-api.update_status(random_sentence())
+    auth = tweepy.OAuthHandler(SECRET_KEY, SECRET_SECRET)
+    auth.set_access_token(SECRET_ACCESS, SECRET_ACCESS_SECRET)
+    api = tweepy.API(auth)
+    return (api)
+
+
+
+if __name__ == "__main__":
+    """
+    Default behavior
+    """
+    from random_sentences import random_sentence
+    api = authenticate()
+    api.update_status(random_sentence())
